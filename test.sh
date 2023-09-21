0c5063889eba76a2ca08e973be98a87701c344f0
magician-blue <143765148+magician-blue@users.noreply.github.com>
2023-09-21 11:35:42 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,7 @@ mojo llama2.mojo tl-chat.bin \
     -n 50 -t 0 -s 100 -i "<|im_start|>user\n"
 echo "============================================================"
 mojo llama2.mojo tl.bin \
+    -tk tok_tl.bin \
     -n 50 -t 0 -s 100 -i "What's the meaning of life?"
 echo "============================================================"
 mojo llama2.mojo stories15M.bin \
