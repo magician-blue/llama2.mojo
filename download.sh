9cf9366af5b18db25687f6ea30fc16ca46dff994
kirp <kirp2199.com>
2023-09-22 17:49:25 -0400

    update the download.sh and test.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,11 @@
 #!bin/bash
-git lfs install
-git clone https://huggingface.co/kirp/TinyLlama-1.1B-Chat-v0.2-bin
-git clone https://huggingface.co/kirp/TinyLlama-1.1B-intermediate-step-240k-503b-bin
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories15M.bin
+wget https://huggingface.co/karpathy/tinyllamas/resolve/main/stories110M.bin
+
+# kirp/TinyLlama-1.1B-Chat-v0.2-bin
+wget https://huggingface.co/kirp/TinyLlama-1.1B-Chat-v0.2-bin/resolve/main/tok_tl-chat.bin
+wget https://huggingface.co/kirp/TinyLlama-1.1B-Chat-v0.2-bin/resolve/main/tl-chat.bin
+
+# this model is so weak
+# git clone https://huggingface.co/kirp/TinyLlama-1.1B-intermediate-step-240k-503b-bin
+
--- test.sh
+++ test.sh
@@ -1,14 +1,14 @@
 #!bin/bash
-mojo llama2.mojo ../llama2.c/tl-chat.bin \
-    -z ../llama2.c/tok_tl-chat.bin \
-    -n 256 -t 0 -s 100 -i "<|im_start|>user\nCan you explain huggingface?<|im_end|>\n<|im_start|>assistant\n"
+mojo llama2.mojo tl-chat.bin \
+    -z tok_tl-chat.bin \
+    -n 50 -t 0 -s 100 -i "<|im_start|>user\nCan you explain huggingface?<|im_end|>\n<|im_start|>assistant\n"
 echo "============================================================"
-mojo llama2.mojo ../llama2.c/tl.bin \
-    -z ../llama2.c/tok_tl.bin \
-    -n 50 -t 0 -s 100 -i "What is the meaning of life?" -fl True
-echo "============================================================"
+# mojo llama2.mojo ../llama2.c/tl.bin \
+#     -z ../llama2.c/tok_tl.bin \
+#     -n 50 -t 0 -s 100 -i "What is the meaning of life?"
+# echo "============================================================"
 mojo llama2.mojo stories15M.bin \
-    -n 100 -t 0 -s 100 -i "Once upon the time,"
+    -n 100 -t 0 -s 100 -i "Once upon the time"
 echo "============================================================"
 mojo llama2.mojo stories110M.bin \
-    -n 100 -t 0 -s 100 -i "Once upon the time,"
+    -n 100 -t 0 -s 100 -i "Once upon the time"
